25fc0c519e2bc528f118d8c0e8c0d6377c122595
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-05-10 11:10:07 +0900

    Put each release pkg into nix files

--- default.nix
+++ default.nix
@@ -3,22 +3,25 @@
 with pkgs;
 
 let
-  fetchFromNixifyRepo = { rev, sha256 }:
-  fetchFromGitHub {
-    owner = "mnacamura";
-    repo = "nixify";
-    inherit rev sha256;
-  };
-in
-
-rec {
   unstable = callPackage ./pkg.nix {};
 
-  release_0_1 = unstable.overrideAttrs (_: rec {
-    version = "0.1";
-    src = fetchFromNixifyRepo {
+  makeRelease = { version, sha256 }:
+  unstable.overrideAttrs (_: {
+    inherit version;
+    src = fetchFromGitHub {
+      owner = "mnacamura";
+      repo = "nixify";
       rev = version;
-      sha256 = "12razszgz0q7h8fsq2n272ldc6zi45ylh3vawbm5ls7jraxi846v";
+      inherit sha256;
     };
   });
+in
+
+{
+  inherit unstable;
+
+  release_0_1 = makeRelease {
+    version = "0.1";
+    sha256 = "12razszgz0q7h8fsq2n272ldc6zi45ylh3vawbm5ls7jraxi846v";
+  };
 }
--- nixify-overlay.nix
+++ nixify-overlay.nix
@@ -1,22 +1,25 @@
 self: super:
 
 let
-  fetchFromNixifyRepo = { rev, sha256 }:
-  self.fetchFromGitHub {
-    owner = "mnacamura";
-    repo = "nixify";
-    inherit rev sha256;
-  };
-in
-
-rec {
-  nixify-unstable = self.callPackage ./pkg.nix {};
-
-  nixify = nixify-unstable.overrideAttrs (_: rec {
-    version = "0.1";
-    src = fetchFromNixifyRepo {
+  makeRelease = { version, sha256 }:
+  self.nixify-unstable.overrideAttrs (_: {
+    inherit version;
+    src = self.fetchFromGitHub {
+      owner = "mnacamura";
+      repo = "nixify";
       rev = version;
-      sha256 = "12razszgz0q7h8fsq2n272ldc6zi45ylh3vawbm5ls7jraxi846v";
+      inherit sha256;
     };
   });
+in
+
+{
+  nixify-unstable = self.callPackage ./pkg.nix {};
+
+  nixify = self.nixify_0_1;
+
+  nixify_0_1 = makeRelease {
+    version = "0.1";
+    sha256 = "12razszgz0q7h8fsq2n272ldc6zi45ylh3vawbm5ls7jraxi846v";
+  };
 }
