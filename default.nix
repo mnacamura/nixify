4eb0839acc3b2475024dbe643548e3ff76caa187
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-05-14 22:03:00 +0900

    Simplify default.nix

--- default.nix
+++ default.nix
@@ -1,37 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
-with pkgs;
-
-let
-  unstable = callPackage ./pkg.nix {};
-
-  makeRelease = { version, sha256 }:
-  unstable.overrideAttrs (_: {
-    inherit version;
-    src = fetchFromGitHub {
-      owner = "mnacamura";
-      repo = "nixify";
-      rev = version;
-      inherit sha256;
-    };
-  });
-in
+with pkgs.extend (import ./nixify-overlay.nix);
 
 {
-  inherit unstable;
+  unstable = nixify-unstable;
 
-  release_0_2_1 = makeRelease {
-    version = "0.2.1";
-    sha256 = "10mmcd5vq47abc2v3rc34v0pla6w636wwl874hmjrgc8skfd321j";
-  };
-
-  release_0_2 = makeRelease {
-    version = "0.2";
-    sha256 = "1hfmlgkvj9dl600q46b7pvbhqx4y2s1ikgfh75h0b3r9qqzxlw9d";
-  };
-
-  release_0_1 = makeRelease {
-    version = "0.1";
-    sha256 = "12razszgz0q7h8fsq2n272ldc6zi45ylh3vawbm5ls7jraxi846v";
-  };
+  release = nixify;
 }
